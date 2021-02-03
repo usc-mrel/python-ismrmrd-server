b9fa990e924646d1012890ec0cbfccb1d889a1bd
Kelvin Chow <kelvin.chow@gmail.com>
2021-02-03 01:14:39 -0600

    Update analyzeflow example to set ImageRowDir/ImageColumnDir and cleanup

--- analyzeflow.py
+++ analyzeflow.py
@@ -52,17 +52,6 @@ def process(connection, config, metadata):
                 if item.image_type is ismrmrd.IMTYPE_PHASE:
                     imgGroup.append(item)
                 else:
-                    # Group these images into separate series (to be fixed in FIRE later)
-                    if meta['ImageType'] == 'ORIGINAL\\PRIMARY\\T1\\NONE':
-                        item.image_series_index = 0
-                    elif meta['ImageType'] == 'ORIGINAL\\PRIMARY\\VELOCITY\\NONE'  and  meta['FlowDirDisplay'] == 'FLOW_DIR_A_TO_P':
-                        item.image_series_index = 1
-                    elif meta['ImageType'] == 'ORIGINAL\\PRIMARY\\VELOCITY\\NONE'  and  meta['FlowDirDisplay'] == 'FLOW_DIR_R_TO_L':
-                        item.image_series_index = 2
-                    elif meta['ImageType'] == 'ORIGINAL\\PRIMARY\\VELOCITY\\NONE'  and  meta['FlowDirDisplay'] == 'FLOW_DIR_TP_IN':
-                        item.image_series_index = 3
-                    elif meta['ImageType'] == 'DERIVED\\PRIMARY\\ANGIO\\ADDITION':
-                        item.image_series_index = 4
 
                     connection.send_image(item)
                     continue
@@ -117,6 +106,7 @@ def process_image(images, config, metadata):
     if 'IceMiniHead' in tmpMeta:
         logging.debug("IceMiniHead[0]: %s", base64.b64decode(tmpMeta['IceMiniHead']).decode('utf-8'))
 
+    # Extract some indices for the images
     slice = [img.slice for img in images]
     phase = [img.phase for img in images]
 
@@ -129,7 +119,7 @@ def process_image(images, config, metadata):
     last_series = 10
     imagesOut = []
     for venc_dir in unique_venc_dir:
-        # data array has dimensions [x y sli phs]
+        # data array has dimensions [row col sli phs], i.e. [y x sli phs]
         # info lists has dimensions [sli phs]
         data = np.zeros((images[0].data.shape[2], images[0].data.shape[3], max(slice)+1, max(phase)+1), images[0].data.dtype)
         head = [[None]*(max(phase)+1) for _ in range(max(slice)+1)]
@@ -137,6 +127,7 @@ def process_image(images, config, metadata):
 
         for img, sli, phs in zip(images, slice, phase):
             if ismrmrd.Meta.deserialize(img.attribute_string)['FlowDirDisplay'] == venc_dir:
+                # print("sli phs", sli, phs)
                 data[:,:,sli,phs] = img.data
                 head[sli][phs]    = img.getHead()
                 meta[sli][phs]    = ismrmrd.Meta.deserialize(img.attribute_string)
@@ -149,7 +140,7 @@ def process_image(images, config, metadata):
         data_meandiff = np.mean(np.abs(np.diff(data,3)),3)
         data_masked = data
         data_masked[(data_meandiff > threshold)] = 2048
-        np.save(debugFolder + "/" + "data_" + venc_dir + ".npy", data_masked)
+        np.save(debugFolder + "/" + "data_masked_" + venc_dir + ".npy", data_masked)
 
         # Normalize and convert to int16
         data_masked = (data_masked.astype(np.float64) - 2048)*32767/2048
@@ -159,6 +150,9 @@ def process_image(images, config, metadata):
         for sli in range(data_masked.shape[2]):
             for phs in range(data_masked.shape[3]):
                 # Create new MRD instance for the processed image
+                # NOTE: from_array() takes input data as [x y z coil], which is
+                # different than the internal representation in the "data" field as
+                # [coil z y x], so we need to transpose
                 tmpImg = ismrmrd.Image.from_array(data_masked[...,sli,phs].transpose())
 
                 # Set the header information
@@ -174,6 +168,14 @@ def process_image(images, config, metadata):
                 tmpMeta['ImageProcessingHistory'] = ['FIRE', 'PYTHON']
                 tmpMeta['WindowCenter']           = '16384'
                 tmpMeta['WindowWidth']            = '32768'
+                tmpMeta['Keep_image_geometry']    = 1
+
+                # Add image orientation directions to MetaAttributes if not already present
+                if tmpMeta.get('ImageRowDir') is None:
+                    tmpMeta['ImageRowDir'] = ["{:.18f}".format(tmpHead.read_dir[0]), "{:.18f}".format(tmpHead.read_dir[1]), "{:.18f}".format(tmpHead.read_dir[2])]
+
+                if tmpMeta.get('ImageColumnDir') is None:
+                    tmpMeta['ImageColumnDir'] = ["{:.18f}".format(tmpHead.phase_dir[0]), "{:.18f}".format(tmpHead.phase_dir[1]), "{:.18f}".format(tmpHead.phase_dir[2])]
 
                 xml = tmpMeta.serialize()
                 logging.debug("Image MetaAttributes: %s", xml)
