ccf285e84ce702a6aee1b02d9533b241a4462055
Kelvin Chow <kelvin.chow@gmail.com>
2021-04-09 02:11:52 -0500

    Minor cleanup of Dockerfiles and startup scripts

--- start-fire-python-server.sh
+++ start-fire-python-server.sh
@@ -5,8 +5,8 @@
 
 if [ $# -eq 1 ]; then
   LOG_FILE=${1}
-  /usr/local/bin/python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 -l=${LOG_FILE} &
+  python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 -l=${LOG_FILE} &
 else
-  /usr/local/bin/python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 &
+  python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 &
 fi
 
--- sync-code-and-start-fire-python-server.sh
+++ sync-code-and-start-fire-python-server.sh
@@ -7,8 +7,8 @@ cp -R -f /tmp/share/code/* "/opt/code/python-ismrmrd-server/"
 
 if [ $# -eq 1 ]; then
   LOG_FILE=${1}
-  /usr/local/bin/python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 -l=${LOG_FILE} &
+  python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 -l=${LOG_FILE} &
 else
-  /usr/local/bin/python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 &
+  python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 &
 fi
 
