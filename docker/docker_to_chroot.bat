c94cbf3d6a3e7f2160bc433246433e3abf14b29c
Kelvin Chow <kelvin.chow@gmail.com>
2022-10-22 20:56:41 -0500

    Fix issue with docker_to_chroot.bat not working

--- docker/docker_to_chroot.bat
+++ docker/docker_to_chroot.bat
@@ -1,27 +1,42 @@
 @echo off
 rem This script takes a Docker image and creates a chroot image (.img)
 rem Note that this script also requires docker_tar_to_chroot.sh to be located in the same folder
-
+ 
 rem Syntax: docker_to_chroot.bat kspacekelvin/fire-python fire-python-chroot.img
-
+ 
+if     "%1"=="" GOTO wrongargnum
+if     "%2"=="" GOTO wrongargnum
+if not "%3"=="" GOTO wrongargnum
+ 
 set DOCKER_NAME=%1
 set CHROOT_FILE=%2
 set EXPORT_FILE=docker-export.tar
-
+ 
+if exist %EXPORT_FILE% (
+  echo Warning -- %EXPORT_FILE% exists and will be overwritten!
+  del %EXPORT_FILE%
+)
+ 
 rem Create a Docker container and export to a .tar file
 echo ------------------------------------------------------------
 echo Exporting Docker image %DOCKER_NAME%
 echo ------------------------------------------------------------
-
+ 
 docker create --name tmpimage %DOCKER_NAME%
 docker export -o %EXPORT_FILE% tmpimage
 docker rm tmpimage
-
+ 
 rem Run a privileged Docker to create the chroot file
 docker run -it --rm          ^
            --privileged=true ^
            -v "%cd%":/share  ^
            ubuntu            ^
-           /bin/bash "sed -i -e 's/\r//g' /share/docker_tar_to_chroot.sh && /share/docker_tar_to_chroot.sh /share/%EXPORT_FILE% /share/%CHROOT_FILE%"
-
+           /bin/bash -c "sed -i -e 's/\r//g' /share/docker_tar_to_chroot.sh && /share/docker_tar_to_chroot.sh /share/%EXPORT_FILE% /share/%CHROOT_FILE%"
+ 
 del %EXPORT_FILE%
+goto eof
+ 
+:wrongargnum
+echo Syntax: docker_to_chroot.bat docker_image_name chroot_file_name
+ 
+:eof
--- docker/docker_to_chroot.sh
+++ docker/docker_to_chroot.sh
@@ -12,6 +12,11 @@ echo ------------------------------------------------------------
 echo Exporting Docker image ${DOCKER_NAME}
 echo ------------------------------------------------------------
 
+if test -f "${EXPORT_FILE}"; then
+    echo "Warning -- ${EXPORT_FILE} exists and will be overwritten!"
+    rm ${EXPORT_FILE}
+fi
+
 docker create --name tmpimage ${DOCKER_NAME}
 docker export -o ${EXPORT_FILE} tmpimage
 docker rm tmpimage
@@ -21,6 +26,6 @@ docker run -it --rm          \
            --privileged=true \
            -v $(pwd):/share  \
            ubuntu            \
-           /bin/bash /share/docker_tar_to_chroot.sh /share/${EXPORT_FILE} /share/${CHROOT_FILE}
+           /bin/bash -c "sed -i -e 's/\r//g' /share/docker_tar_to_chroot.sh && /share/docker_tar_to_chroot.sh /share/${EXPORT_FILE} /share/${CHROOT_FILE}"
 
 rm ${EXPORT_FILE}
