b9792e6deddc1f0b9b9dcc7546460417eb31003b
Kelvin Chow <kelvin.chow@gmail.com>
2024-02-20 16:51:58 -0700

    Add rendering of ROIs in mrd2gif

--- mrd2gif.py
+++ mrd2gif.py
@@ -5,7 +5,8 @@ import argparse
 import h5py
 import ismrmrd
 import numpy as np
-from PIL import Image
+import mrdhelper
+from PIL import Image, ImageDraw
 
 def main(args):
     dset = h5py.File(args.filename, 'r')
@@ -63,6 +64,7 @@ def main(args):
         print("Reading images from '/" + args.in_group + "/" + group + "'")
 
         images = []
+        rois   = []
         for imgNum in range(0, dset.number_of_images(group)):
             image = dset.read_image(group, imgNum)
 
@@ -77,19 +79,48 @@ def main(args):
                         data = np.squeeze(image.data[cha,sli,...]) # image.data is [cha z y x] -- squeeze to [y x] for [row col]
                         images.append(Image.fromarray(data))
 
+            # Read ROIs
+            meta = ismrmrd.Meta.deserialize(image.attribute_string)
+            imgRois = []
+            for key in meta.keys():
+                if not key.startswith('ROI_'):
+                    continue
+
+                roi = meta[key]
+                x, y, rgb, thickness, style, visibility = mrdhelper.parse_roi(roi)
+
+                if visibility == 0:
+                    continue
+
+                imgRois.append((x, y, rgb, thickness))
+            rois.append(imgRois)
         print("  Read in %s images" % (len(images)))
+
+        hasRois = any([len(x) > 0 for x in rois])
 
         # Window/level images
         maxVal = np.median([np.percentile(np.array(img), 95) for img in images])
         minVal = np.median([np.percentile(np.array(img),  5) for img in images])
 
         imagesWL = []
-        for img in images:
+        for img, roi in zip(images, rois):
             if img.mode != 'RGB':
-                data = np.array(img).astype(float)
-                data -= minVal
-                data *= 255/(maxVal - minVal)
-                imagesWL.append(Image.fromarray(data))
+                if hasRois:
+                    # Convert to RGB mode to allow colored ROI overlays
+                    data = np.array(img).astype(float)
+                    data -= minVal
+                    data *= 128/(maxVal - minVal)
+                    tmpImg = Image.fromarray(np.repeat(data[:,:,np.newaxis],3,axis=2).astype(np.uint8), mode='RGB')
+
+                    draw = ImageDraw.Draw(tmpImg)
+                    for (x, y, rgb, thickness) in roi:
+                        draw.line(list(zip(x, y)), fill=(int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255), 0), width=int(thickness))
+                    imagesWL.append(tmpImg)
+                else:
+                    data = np.array(img).astype(float)
+                    data -= minVal
+                    data *= 255/(maxVal - minVal)
+                    imagesWL.append(Image.fromarray(data))
             else:
                 imagesWL.append(img)
 
